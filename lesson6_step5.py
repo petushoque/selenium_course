81d6d14602751a2881c719d319a09c1a57b9df89
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-07 16:07:25 +0700

    Create lesson6_step5.py

---
+++ lesson6_step5.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import time
+import math
+
+link = "http://suninjuly.github.io/find_link_text"
+text_to_find = str(math.ceil(math.pow(math.pi, math.e)*10000))
+
+try:
+    #открыть браузер, перейти по ссылке
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    link_founded = browser.find_element_by_link_text(text_to_find)
+    link_founded.click()
+
+    input1 = browser.find_element_by_tag_name("input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name("last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name("city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element_by_id("country")
+    input4.send_keys("Russia")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
