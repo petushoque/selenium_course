153859fb5543c9c00f73a626d3a535cd5532dc1f
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-07 16:33:01 +0700

    Update lesson6_step8.py

--- lesson6_step8.py
+++ lesson6_step8.py
@@ -15,7 +15,7 @@ try:
     input3.send_keys("Smolensk")
     input4 = browser.find_element_by_id("country")
     input4.send_keys("Russia")
-    button = browser.find_element_by_css_xpath("/html/body/div/form/div[6]/button[3]")
+    button = browser.find_element_by_xpath("/html/body/div/form/div[6]/button[3]")
     button.click()
 
 finally:
