c3c80954f1adb1fb2d327757477fe8babcc5c10a
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-07 17:08:04 +0700

    Create lesson6_step11.py

---
+++ lesson6_step11.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+
+    elements = browser.find_elements_by_tag_name("input")
+    for element in elements:
+        element.send_keys("Привет, мир")
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+#пустая строка в конце
