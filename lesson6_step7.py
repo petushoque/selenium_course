fe495334ca54813074301e9a168eac7e77532def
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-07 16:21:01 +0700

    Create lesson6_step7.py

---
+++ lesson6_step7.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/huge_form.html")
+    elements = browser.find_elements_by_tag ("input")
+    for element in elements:
+        element.send_keys("Привет, мир")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
