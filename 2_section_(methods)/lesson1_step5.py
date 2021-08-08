65f5403e70a7dbab4699acc04c329f56b8d2c07f
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-08 09:45:54 +0700

    Update lesson1_step5.py

--- 2_section_(methods)/lesson1_step5.py
+++ 2_section_(methods)/lesson1_step5.py
@@ -24,6 +24,10 @@ try:
     input_answer = browser.find_element_by_id("answer")
     input_answer.send_keys(result)
 
+    # Отметить чекбокс
+    robot_checkbox = browser.find_element_by_id("robotCheckbox")
+    robot_checkbox.click()
+
     # Отправляем заполненную форму
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
