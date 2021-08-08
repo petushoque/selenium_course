ebaba28ab0591c0b57b87e333978045b4befa64c
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-08 09:17:27 +0700

    Create lesson1_step5.py

---
+++ 2_section_(methods)/lesson1_step5.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+
+    first_block_first = browser.find_element_by_css_selector(".first_block .first")
+    first_block_first.send_keys("Test first name")
+    first_block_second = browser.find_element_by_css_selector(".first_block .second")
+    first_block_second.send_keys("Test last name")
+    first_block_third = browser.find_element_by_css_selector(".first_block .third")
+    first_block_third.send_keys("Test email")
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+#пустая строка в конце
