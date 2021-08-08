915ce95f83530bd190d712819e2ae0bd92df48c1
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-08 09:39:10 +0700

    Update lesson1_step5.py

--- 2_section_(methods)/lesson1_step5.py
+++ 2_section_(methods)/lesson1_step5.py
@@ -14,25 +14,16 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # Основная часть кода, клики по чекбоксам и радиокнопкам
+    # Найти вводные данные для вычисления
+    x_value = browser.find_element_by_id("input_value")
+
+    # Найти поле ввода ответа
     input_answer = browser.find_element_by_id("answer")
-    input_answer.send_keys("Test first name")
+    input_answer.send_keys(x_value)
 
     # Отправляем заполненную форму
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
-
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
-
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
@@ -40,4 +31,4 @@ finally:
     # закрываем браузер после всех манипуляций
     browser.quit()
 
-#пустая строка в конце
+#спустая строка в конце
