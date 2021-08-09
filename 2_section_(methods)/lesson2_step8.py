49d446a523754c13675d4010843b7e399ba52866
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-09 21:24:58 +0700

    Update lesson2_step8.py

--- 2_section_(methods)/lesson2_step8.py
+++ 2_section_(methods)/lesson2_step8.py
@@ -9,24 +9,9 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # Найти вводные данные для вычисления
-    x_value = browser.find_element_by_id("input_value")
-
-    # Вычислить значение выражения
-    result = calc(x_value.text)
-
     # Найти панель ввода результата и отправить результат вычислений
-    answer_field = browser.find_element_by_id("answer")
-    answer_field.send_keys(result)
-
-    # Найти чекбокс, пролистать до него страницу и поставить флажок
-    robot_checkbox = browser.find_element_by_id("robotCheckbox")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", robot_checkbox)
-    robot_checkbox.click()
-
-    # Отметить радиокнопку
-    robot_radio = browser.find_element_by_id("robotsRule")
-    robot_radio.click()
+    input_first_name = browser.find_element_by_name("firstname")
+    input_first_name.send_keys("Hello")
 
     # Найти кнопку и отправить ответ
     button = browser.find_element_by_tag_name("button")
