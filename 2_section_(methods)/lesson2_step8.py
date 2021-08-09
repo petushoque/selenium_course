3c932a06a03c9adc949a92a21473f27163efd2f1
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-09 21:36:19 +0700

    Update lesson2_step8.py

--- 2_section_(methods)/lesson2_step8.py
+++ 2_section_(methods)/lesson2_step8.py
@@ -18,7 +18,9 @@ try:
     input_email.send_keys("email@email.ru")
 
     # Загрузить фаил
-    input_file = browser.find_element_by_name("file")
+    current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла 
+    file_path = os.path.join(current_dir, 'file.txt')   
+    input_file = browser.find_element_by_name(file_path)
     
 
     # Найти кнопку и отправить ответ
