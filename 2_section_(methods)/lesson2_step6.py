705975786bf973a7ed4f33b4cb99795ce07b31dc
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-09 11:18:58 +0700

    Create lesson2_step6.py

---
+++ 2_section_(methods)/lesson2_step6.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+link = "http://suninjuly.github.io/selects1.html"
+
+try:
+    # Открыть страницу    
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Найти вводные данные для вычисления
+    browser.execute_script("alert('Robots at work');")
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+#спустая строка в конце
