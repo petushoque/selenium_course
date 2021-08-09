25c2bfd84f22145a9986344606a332bc4cf97a7c
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-09 21:30:58 +0700

    Update lesson2_step8.py

--- 2_section_(methods)/lesson2_step8.py
+++ 2_section_(methods)/lesson2_step8.py
@@ -9,9 +9,13 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # Найти панель ввода результата и отправить результат вычислений
+    # Заполнить поля
     input_first_name = browser.find_element_by_name("firstname")
     input_first_name.send_keys("Hello")
+    input_last_name = browser.find_element_by_name("lastname")
+    input_last_name.send_keys("World")
+    input_email = browser.find_element_by_name("email")
+    input_email.send_keys("email@email.ru")
 
     # Найти кнопку и отправить ответ
     button = browser.find_element_by_tag_name("button")
