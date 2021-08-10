76fab92878b42afa69d1608aafb6116c2d98a7fe
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-10 10:58:28 +0700

    Update lesson3_step6.py

--- 2_section_(methods)/lesson3_step6.py
+++ 2_section_(methods)/lesson3_step6.py
@@ -7,20 +7,16 @@ import math
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
-link = "http://suninjuly.github.io/alert_accept.html"
+link = "http://suninjuly.github.io/redirect_accept.html"
 
 try:
     # Открыть страницу    
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # Найти и нажать на основную кнопку
-    button = browser.find_element_by_css_selector("button")
+    # Найти и нажать прыгающую кнопку
+    button = browser.find_element_by_css_selector("button.trollface")
     button.click()
-
-    # Найти и принять alert
-    alert = browser.switch_to.alert
-    alert.accept()
 
     # Найти вводные данные для вычисления
     x_value = browser.find_element_by_id("input_value")
