ee722e5272be6a26bc290da2c23d100e1ee3be07
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-10 10:52:12 +0700

    Update lesson3_step4.py

--- 2_section_(methods)/lesson3_step4.py
+++ 2_section_(methods)/lesson3_step4.py
@@ -30,7 +30,11 @@ try:
 
     # Найти поле ввода ответа
     input_answer = browser.find_element_by_id("answer")
-    input_answer.send_keys(result) 
+    input_answer.send_keys(result)
+
+    # Найти кнопку submit и отправить ответ
+    confirm_button = browser.find_element_by_css_selector("button.btn-primary")
+    confirm_button.click()
     
 finally:
     # Десятичекундная пауза
