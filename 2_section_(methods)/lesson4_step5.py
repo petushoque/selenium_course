792fd1fd85fcd8cb6aa91a4a6ee4b0771f635d0c
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-10 21:38:39 +0700

    Update lesson4_step5.py

--- 2_section_(methods)/lesson4_step5.py
+++ 2_section_(methods)/lesson4_step5.py
@@ -4,17 +4,16 @@ browser = webdriver.Chrome()
 # говорим WebDriver искать каждый элемент в течение 5 секунд
 browser.implicitly_wait(5)
 
-browser.get("http://suninjuly.github.io/wait1.html")
+try:
+  browser.get("http://suninjuly.github.io/wait1.html")
 
-button = browser.find_element_by_id("verify")
-button.click()
-message = browser.find_element_by_id("verify_message")
+  button = browser.find_element_by_id("verify")
+  button.click()
+  message = browser.find_element_by_id("verify_message")
 
-assert "successful" in message.text
+  assert "successful" in message.text
     
 finally:
-    # Десятичекундная пауза
-    time.sleep(10)
     # Закрыть браузер
     browser.quit()
 
