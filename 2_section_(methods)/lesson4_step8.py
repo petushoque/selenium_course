f87d570114fef64ed802becdb5146212b31d01a3
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-11 08:40:00 +0700

    Update lesson4_step8.py

--- 2_section_(methods)/lesson4_step8.py
+++ 2_section_(methods)/lesson4_step8.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+import math
 import time
 
 # Функция для вычисления значения выражения
