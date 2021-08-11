1036f368f90f4e2d380d7d1f2ecd3bbf26b52b4b
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-11 08:06:18 +0700

    Update lesson4_step8.py

--- 2_section_(methods)/lesson4_step8.py
+++ 2_section_(methods)/lesson4_step8.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 link = "http://suninjuly.github.io/explicit_wait2.html"
@@ -7,13 +10,12 @@ browser = webdriver.Chrome()
 browser.implicitly_wait(5)
 
 try:
+  # Открыть страницу по ссылке
   browser.get(link)
 
-  button = browser.find_element_by_id("verify")
-  button.click()
-  message = browser.find_element_by_id("verify_message")
+  price = WebDriverWait(browser, 12).until(
+    EC.text_to_be_present_in_element((By.ID, "price"), )
 
-  assert "successful" in message.text
     
 finally:
     # Закрыть браузер
