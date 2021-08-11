770e165dc3b42a011c54c22c2138932c54a1c070
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-11 08:20:04 +0700

    Update lesson4_step8.py

--- 2_section_(methods)/lesson4_step8.py
+++ 2_section_(methods)/lesson4_step8.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+import time
 
 link = "http://suninjuly.github.io/explicit_wait2.html"
 
@@ -16,11 +17,14 @@ try:
   price = WebDriverWait(browser, 12).until(
     EC.text_to_be_present_in_element((By.ID, "price"), "100"))
 
+  print(price)
+
   book_button = browser.find_element_by_id("book")
   book_button.click()
 
     
 finally:
+  time.sleep(5)
     # Закрыть браузер
     browser.quit()
 
