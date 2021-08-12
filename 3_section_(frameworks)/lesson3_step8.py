c2575d6c70cd86e76f1d56d9b2f280e3042929e4
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-12 09:31:28 +0700

    Update lesson3_step8.py

--- 3_section_(frameworks)/lesson3_step8.py
+++ 3_section_(frameworks)/lesson3_step8.py
@@ -1,8 +1,7 @@
-# Покажет ошибку AssertionError
-assert abs(-42) == -42
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
 
-# Покажет указанное сообщение к ошибке,
-# "Should be absolute value of a number"
-assert abs(-42) == -42, "Should be absolute value of a number"
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
 
 # не забываем оставить пустую строку в конце файла
