b60f5e32cc89f307c6fdb0cc6e9c4f5736cb49bb
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-12 07:18:58 +0700

    Update lesson2_step9.py

--- 3_section_(frameworks)/lesson2_step9.py
+++ 3_section_(frameworks)/lesson2_step9.py
@@ -3,4 +3,4 @@
 # в случае несовпадения, предоставить исчерпывающее сообщение об ошибке. 
 
 def test_input_text(expected_result, actual_result):
-    assert 
+    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
