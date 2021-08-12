af2e0aeb03e9bf57b594e27ebd285d123552973b
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-12 07:43:23 +0700

    Update lesson2_step13.py

--- 3_section_(frameworks)/lesson2_step13.py
+++ 3_section_(frameworks)/lesson2_step13.py
@@ -4,10 +4,22 @@ import time
 
 class TestRegistration(unittest.TestCase):
     def test_registration_page_1(self):
-        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
         
-    def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        elements = browser.find_elements_by_tag_name("input")
+        for element in elements:
+            element.send_keys("Привет, мир")
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Should be absolute value of a number")
+        
+    #def test_abs2(self):
+    #    self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
         
 if __name__ == "__main__":
     unittest.main()
