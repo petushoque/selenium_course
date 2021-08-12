5f33892e2891f830438e949bba74f377812404b1
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-12 07:07:58 +0700

    Update lesson2_step8.py

--- 3_section_(frameworks)/lesson2_step8.py
+++ 3_section_(frameworks)/lesson2_step8.py
@@ -1,2 +1,5 @@
+# Функция должна проверить совпадение значений с помощью оператора assert и,
+# в случае несовпадения, предоставить исчерпывающее сообщение об ошибке. 
+
 def test_input_text(expected_result, actual_result):
     assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
