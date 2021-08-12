c93e3b7e7b793f60d871649c01da7a2f93f577b1
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-12 07:06:59 +0700

    Update lesson2_step8.py

--- 3_section_(frameworks)/lesson2_step8.py
+++ 3_section_(frameworks)/lesson2_step8.py
@@ -1,8 +1,2 @@
-# Покажет ошибку AssertionError
-assert abs(-42) == -42
-
-# Покажет указанное сообщение к ошибке,
-# "Should be absolute value of a number"
-assert abs(-42) == -42, "Should be absolute value of a number"
-
-# не забываем оставить пустую строку в конце файла
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
