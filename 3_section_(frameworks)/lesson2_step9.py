73549e20cde26574922d0e3a9c355933333e3525
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-12 07:13:26 +0700

    Update lesson2_step9.py

--- 3_section_(frameworks)/lesson2_step9.py
+++ 3_section_(frameworks)/lesson2_step9.py
@@ -2,4 +2,4 @@
 # в случае несовпадения, предоставить исчерпывающее сообщение об ошибке. 
 
 def test_input_text(expected_result, actual_result):
-    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+    assert 
