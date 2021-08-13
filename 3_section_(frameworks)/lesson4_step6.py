60eb25b65810f763ade4ea8a0a7973b507999730
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-13 08:19:38 +0700

    Update lesson4_step6.py

--- 3_section_(frameworks)/lesson4_step6.py
+++ 3_section_(frameworks)/lesson4_step6.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
@@ -12,20 +12,20 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("preparing some critical data for every test")
+
 
 class TestMainPage1():
-
-    # вызываем фикстуру в тесте, передав ее как параметр
     def test_guest_should_see_login_link(self, browser):
-        print("start test1")
+        # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
-        print("finish test1")
 
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        print("start test2")
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
-        print("finish test2")
 
 # не забываем оставить пустую строку в конце файла
