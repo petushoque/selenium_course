9065c6d062ee92a5686766c72cd45b2dce650630
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-13 08:17:41 +0700

    Update lesson4_step5.py

--- 3_section_(frameworks)/lesson4_step5.py
+++ 3_section_(frameworks)/lesson4_step5.py
@@ -4,24 +4,28 @@ from selenium import webdriver
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture
+@pytest.fixture(scope="class")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
-    # этот код выполнится после завершения теста
     print("\nquit browser..")
     browser.quit()
 
 
 class TestMainPage1():
+
     # вызываем фикстуру в тесте, передав ее как параметр
     def test_guest_should_see_login_link(self, browser):
+        print("start test1")
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
+        print("finish test1")
 
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        print("start test2")
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+        print("finish test2")
 
 # не забываем оставить пустую строку в конце файла
