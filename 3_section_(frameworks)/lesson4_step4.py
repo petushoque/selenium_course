3c0cff77762caaa01734714891c07612031ffdc8
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-13 08:13:39 +0700

    Update lesson4_step4.py

--- 3_section_(frameworks)/lesson4_step4.py
+++ 3_section_(frameworks)/lesson4_step4.py
@@ -3,11 +3,16 @@ from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/"
 
+
 @pytest.fixture
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    return browser
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    browser.quit()
+
 
 class TestMainPage1():
     # вызываем фикстуру в тесте, передав ее как параметр
