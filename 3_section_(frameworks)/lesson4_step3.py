1c4d04ff7129832d8de0d857c64741ec84713650
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-13 07:59:22 +0700

    Create lesson4_step3.py

---
+++ 3_section_(frameworks)/lesson4_step3.py
@@ -0,0 +1,7 @@
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
+# не забываем оставить пустую строку в конце файла
