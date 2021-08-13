ad3f3653593818a5cd32f3b8a4a4c23ebd909199
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-08-13 08:01:20 +0700

    Update lesson4_step3.py

--- 3_section_(frameworks)/lesson4_step3.py
+++ 3_section_(frameworks)/lesson4_step3.py
@@ -1,7 +1,22 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number"
+import pytest
+from selenium import webdriver
 
-def test_abs2():
-    assert abs(-42) == -42, "Should be absolute value of a number"
+link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    return browser
+
+class TestMainPage1():
+    # вызываем фикстуру в тесте, передав ее как параметр
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
 
 # не забываем оставить пустую строку в конце файла
